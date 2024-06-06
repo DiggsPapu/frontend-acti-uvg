19d478af89b5331469e2d5d9debdb228e6e39ea7
DiggsPapu <diggs.papu@gmail.com>
2024-06-06 22:23:58 +0000

    crear: Todas las configuraciones en vite
    Se crearon todas las configuraciones y demas para tener un frontend en react usando vite

---
+++ README.md
@@ -0,0 +1,2 @@
+# frontend-acti-uvg
+Frontend para el manejo de horas de extensión en la UVG
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+// import { resolve } from 'path'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/Acti/',
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: '1404',
+  },
+  // resolve: {
+  //   alias: [
+  //     { find: '@components', replacement: resolve(__dirname, '/src/components') },
+  //     { find: '@assets', replacement: resolve(__dirname, '/src/assets') },
+  //     { find: '@pages', replacement: resolve(__dirname, '/src/pages') },
+  //   ],
+  // },
+})
