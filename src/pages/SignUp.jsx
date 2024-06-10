424ff3856aca386265a11b26c8d4f0df449312c1
DiggsPapu <diggs.papu@gmail.com>
2024-06-10 21:05:33 +0000

    actualizacion: Form Estudiante
    Se desarrollo el form para el estudiante, aun no tiene la parte de services para enviar la informacion debido a que el backend no existe pero ahi va

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,14 +1,23 @@
+
+
+import * as React from 'react';
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import { useState } from "react"
 import { 
   Button, 
   createTheme, 
   ThemeProvider, 
   Stepper,
   Step,
-  StepLabel 
+  StepLabel, 
+  StepButton,
+  TextField
 } from "@mui/material"
 import { useNavigate } from "react-router-dom"
 import SchoolIcon from '@mui/icons-material/School'
 import CorporateFareIcon from '@mui/icons-material/CorporateFare'
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace'
 import Title from '../components/Title'
 import style from './Pages.module.css'
 
@@ -30,7 +39,7 @@ const steps = [
   'Afiliación',
   'Datos',
   'Confirmación',
-];
+]
 const buttonTheme = createTheme({
   typography: {
     fontFamily: 'RobotoMono',
@@ -46,10 +55,60 @@ const buttonTheme = createTheme({
   },
 })
 
-function SignUp() {
-    const navigate = useNavigate()
-    return (
-      <div className={style.pageStructure}>
+export default function SignUp() {
+  const [activeStep, setActiveStep] = useState(0)
+  const [completed, setCompleted] = useState({})
+  const [afiliation, setAfiliation] = useState(0)
+  const navigate = useNavigate()
+
+  const totalSteps = () => {
+    return steps.length
+  };
+
+  const completedSteps = () => {
+    return Object.keys(completed).length
+  };
+
+  const isLastStep = () => {
+    return activeStep === totalSteps() - 1
+  };
+
+  const allStepsCompleted = () => {
+    return completedSteps() === totalSteps()
+  }
+
+  const handleNext = () => {
+    const newActiveStep =
+      isLastStep() && !allStepsCompleted()
+        ? // It's the last step, but not all steps have been completed,
+          // find the first step that has been completed
+          steps.findIndex((step, i) => !(i in completed))
+        : activeStep + 1
+    setActiveStep(newActiveStep)
+  }
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1)
+  }
+
+  const handleStep = (step) => () => {
+    setActiveStep(step)
+  }
+
+  const handleComplete = () => {
+    const newCompleted = completed
+    newCompleted[activeStep] = true
+    setCompleted(newCompleted)
+    handleNext()
+  }
+
+  const handleReset = () => {
+    setActiveStep(0)
+    setCompleted({})
+  }
+
+  return (
+    <Box sx={{ width: '100%' }}>
       <h1 style={{ 
         fontFamily: 'RobotoMono', 
         fontSize: '8vw', 
@@ -70,7 +129,7 @@ function SignUp() {
             marginRight: 'auto',
           }}
           >
-          <Stepper>
+          <Stepper activeStep={activeStep}>
           {steps.map((label) => (
             <Step key={label}>
               <StepLabel>{label}</StepLabel>
@@ -78,61 +137,172 @@ function SignUp() {
           ))}
         </Stepper>
         </div>
-        {/* mini form */}
         <div className={style.miniForm}>
-          {/* Title div */}
-          <div className={style.titleDiv}>
-            <Title content = "Registrate" color='#505862'/>
-          </div>
-          <div className={style.fillDiv}>
-            <ThemeProvider theme={textFieldTheme}>
-              <Button 
-                variant="outlined" 
-                onClick={() => {navigate(-1)}}
-                startIcon={<SchoolIcon />}
-              >
-                Estudiante
-              </Button>  
-              <Button 
-                variant="outlined" 
-                onClick={() => {navigate(-1)}}
-                startIcon={<CorporateFareIcon />}
-              >Organización</Button>  
-            </ThemeProvider>
-          </div>
-          <div style={{
-            width: '40%',
-            height: 'auto',
-            display: 'flex',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-            flexDirection: 'column',
-            justifyContent: 'space-evenly',
-          }}>
-            <ThemeProvider theme={buttonTheme}>
-              <Button variant="contained" onClick={() => {navigate(-1)}}>Cancelar</Button>  
-            </ThemeProvider>
-          </div>
-          <div style={{
-            fontFamily: "Raleway", 
-            display: 'flex', 
-            flexDirection: 'column', 
-            maxWidth: 'fit-content',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-            height: '10vh',
-            justifyContent: 'space-evenly',
-            fontSize: '1vw',
-            marginTop: '5%', 
-            color: '#367D39',
-            textDecoration: 'underline', 
-            }}
-            >
-              <p style={{ cursor: 'pointer',}} onClick={() => {navigate(-1)}}>¿Ya eres miembro? Iniciar Sesión</p>
-          </div>
-        </div>
+        {allStepsCompleted() ? (
+          <React.Fragment>
+            <Typography sx={{ mt: 2, mb: 1 }}>
+              All steps completed - you&apos;re finished
+            </Typography>
+            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+              <Box sx={{ flex: '1 1 auto' }} />
+              <Button onClick={handleReset}>Reset</Button>
+            </Box>
+          </React.Fragment>
+        ) : (
+          <>
+          {
+            activeStep===0?
+            (<>
+              {/* Title div */}
+              <div className={style.titleDiv}>
+                <Title content = "Registrate" color='#505862'/>
+              </div>
+              <div className={style.fillDiv}>
+                <ThemeProvider theme={textFieldTheme}>
+                  <Button 
+                    variant="outlined" 
+                    onClick={() => {
+                      setActiveStep(activeStep+1)
+                      setAfiliation(1)
+                      }}
+                    startIcon={<SchoolIcon />}
+                  >
+                    Estudiante
+                  </Button>  
+                  <Button 
+                    variant="outlined" 
+                    onClick={() => {
+                      setActiveStep(activeStep+1)
+                      setAfiliation(0)
+                      }}
+                    startIcon={<CorporateFareIcon />}
+                  >Organización</Button>  
+                </ThemeProvider>
+              </div>
+              <div style={{
+                width: '40%',
+                height: 'auto',
+                display: 'flex',
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                flexDirection: 'column',
+                justifyContent: 'space-evenly',
+              }}>
+                <ThemeProvider theme={buttonTheme}>
+                  <Button variant="contained" onClick={() => {navigate(-1)}}>Cancelar</Button>  
+                </ThemeProvider>
+              </div>
+              <div style={{
+                fontFamily: "Raleway", 
+                display: 'flex', 
+                flexDirection: 'column', 
+                maxWidth: 'fit-content',
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                height: '10vh',
+                justifyContent: 'space-evenly',
+                fontSize: '1vw',
+                marginTop: '5%', 
+                color: '#367D39',
+                textDecoration: 'underline', 
+                }}
+                >
+                  <p style={{ cursor: 'pointer',}} onClick={() => {navigate(-1)}}>¿Ya eres miembro? Iniciar Sesión</p>
+              </div>
+            </>)
+            :
+            (
+              <>
+              {
+                afiliation ?
+                (
+                  <div style={{display: 'flex', flexDirection: 'column'}}>
+                  <Button 
+                    variant="text" 
+                    onClick={() => { setActiveStep(activeStep-1) }}
+                    startIcon={<KeyboardBackspaceIcon />}
+                  >Regresar</Button>
+                    {/* Title div */}
+                    <div className={style.titleDiv}>
+                      <Title content = "Estudiante" color='#505862'/>
+                    </div>
+                    <div style={{
+                      display: 'grid',
+                      gridTemplateColumns: 'auto auto'
+                    }}>
+                    <TextField 
+                      id="outlined-basic" 
+                      label="Apellidos" 
+                      required 
+                    />
+                    <TextField 
+                      id="outlined-basic" 
+                      label="Nombres" 
+                      required 
+                    />
+                    <TextField 
+                      id="outlined-basic" 
+                      label="Correo UVG" 
+                      required 
+                    />
+                    <TextField 
+                      id="outlined-basic" 
+                      label="Carnet" 
+                      required 
+                    />
+                    <TextField 
+                      id="outlined-basic" 
+                      label="Contraseña" 
+                      required 
+                    />
+                    <TextField 
+                      id="outlined-basic" 
+                      label="Confirmar contraseña" 
+                      required 
+                    />
+                    <TextField 
+                      id="outlined-basic" 
+                      label="Fecha de nacimiento" 
+                      required 
+                    />
+                    <TextField 
+                      id="outlined-basic" 
+                      label="Telefono" 
+                      required 
+                    />
+                    </div>
+                    <TextField 
+                      id="outlined-basic" 
+                      label="Facultad" 
+                      required 
+                    />
+                    <TextField 
+                      id="outlined-basic" 
+                      label="Carrera" 
+                      required 
+                    />
+                    <ThemeProvider theme={buttonTheme}>
+                      <Button variant="contained" onClick={() => {navigate(-1)}}>Continuar</Button>  
+                    </ThemeProvider>
+                  </div>
+                  
+                ) :(
+                  <>
+                    {/* Title div */}
+                    <div className={style.titleDiv}>
+                      <Title content = "Organización" color='#505862'/>
+                    </div>
+                  </>
+              
+                )
+              }
+              </>
+            )
+          }
+          </>
+        )}
       </div>
-    )
-  }
+    </Box>
+  );
+}
 
-export default SignUp
